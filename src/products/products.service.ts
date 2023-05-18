c9519e98750d69f3502ef709c83a6828799eda48
pSakowski <p.sakowski1213@gmail.com>
2023-05-18 23:15:12 +0200

    allow include order info to Products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -8,14 +8,14 @@ export class ProductsService {
 
   public async getAll(): Promise<Product[]> {
     return this.prismaService.product.findMany({
-      include: { gallery: true },
+      include: { orders: true },
     });
   }
 
   public getById(id: Product['id']): Promise<Product | null> {
     return this.prismaService.product.findUnique({
       where: { id },
-      include: { gallery: true },
+      include: { orders: true },
     });
   }
 
