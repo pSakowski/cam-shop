2f14f3843e7203ee3830deb5bef0d5dfcea1d8f0
pSakowski <p.sakowski1213@gmail.com>
2023-05-19 15:34:43 +0200

    add login & authentication strategy with passport package

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { ConflictException, Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { Password, User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -16,7 +16,9 @@ export class UsersService {
     });
   }
 
-  public getByEmail(email: User['email']): Promise<User | null> {
+  public async getByEmail(
+    email: User['email'],
+  ): Promise<(User & { password: Password }) | null> {
     return this.prismaService.user.findUnique({
       where: { email },
       include: { password: true },
