a95e45b3ec0b7aa3fde5c32446eafeb06898ba23
pSakowski <p.sakowski1213@gmail.com>
2023-05-19 15:12:22 +0200

    add /register endpoint with bcryptjs package

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { RegisterDTO } from './dtos/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  public async register(registrationData: RegisterDTO) {
+    const hashedPassword = await bcrypt.hash(registrationData.password, 10);
+    const userData = {
+      email: registrationData.email,
+    };
+    return this.usersService.create(userData, hashedPassword);
+  }
 }
