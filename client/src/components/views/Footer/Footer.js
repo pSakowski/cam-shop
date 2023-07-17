2d483b4d19b3110b79615bfa1da073603729bf25
pSakowski <p.sakowski1213@gmail.com>
2023-07-17 19:16:49 +0200

    fix styles & upgrade Footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,59 +1,189 @@
+
+
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
 const Footer = () => (
-  <footer id="sticky-footer" className={`${styles.footer} py-3`}>
-    <Container>
-      <Row>
-        <Col md="6">
-          <div className={styles.footerSection}>
-            <h5 className={styles.footerSectionTitle}>Join us</h5>
-            <p className={styles.footerSectionText}>
-              Be part of our community and receive the latest updates, offers, and news.
+  <footer className={styles.footer}>
+    <Row className={styles.newsletterRow}>
+      <Col md="1">
+        <h5>Subscribe to our newsletter:</h5>
+        <form>
+          <Row>
+            <Col md="4">
+              <input
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                className={styles.newsletterInput}
+              />
+            </Col>
+            <Col md="4">
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                className={styles.newsletterInput}
+              />
+            </Col>
+            <Col md="4">
+              <button type="submit" className={styles.newsletterButton}>
+                Send
+              </button>
+            </Col>
+          </Row>
+        </form>
+      </Col>
+    </Row>
+    <Row>
+      <Col md="3">
+        <div className={styles.footerSection}>
+          <h5 className={styles.footerSectionTitle}>Help</h5>
+          <ul className={styles.footerList}>
+            <li>
+              <Link to="/contact" className={styles.footerLink}>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/how-to-buy" className={styles.footerLink}>
+                How to Buy?
+              </Link>
+            </li>
+            <li>
+              <Link to="/regulations" className={styles.footerLink}>
+                Regulations
+              </Link>
+            </li>
+            <li>
+              <Link to="/copyright" className={styles.footerLink}>
+                Copyright
+              </Link>
+            </li>
+            <li>
+              <Link to="/privacy-policy" className={styles.footerLink}>
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link to="/cookies" className={styles.footerLink}>
+                Cookies
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Col>
+      <Col md="3">
+        <div className={styles.footerSection}>
+          <h5 className={styles.footerSectionTitle}>Information</h5>
+          <ul className={styles.footerList}>
+            <li>
+              <Link to="/costs-delivery" className={styles.footerLink}>
+                Costs and Delivery Method
+              </Link>
+            </li>
+            <li>
+              <Link to="/payment-methods" className={styles.footerLink}>
+                Payment Methods
+              </Link>
+            </li>
+            <li>
+              <Link to="/exchanges-returns" className={styles.footerLink}>
+                Exchanges and Returns
+              </Link>
+            </li>
+            <li>
+              <Link to="/complaints" className={styles.footerLink}>
+                Complaints
+              </Link>
+            </li>
+            <li>
+              <Link to="/order-status" className={styles.footerLink}>
+                Order Status
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Col>
+      <Col md="3">
+        <div className={styles.footerSection}>
+          <h5 className={styles.footerSectionTitle}>My account</h5>
+          <ul className={styles.footerList}>
+            <li>
+              <Link to="/costs-delivery" className={styles.footerLink}>
+                Costs and Delivery Method
+              </Link>
+            </li>
+            <li>
+              <Link to="/payment-methods" className={styles.footerLink}>
+                Payment Methods
+              </Link>
+            </li>
+            <li>
+              <Link to="/exchanges-returns" className={styles.footerLink}>
+                Exchanges and Returns
+              </Link>
+            </li>
+            <li>
+              <Link to="/complaints" className={styles.footerLink}>
+                Complaints
+              </Link>
+            </li>
+            <li>
+              <Link to="/order-status" className={styles.footerLink}>
+                Order Status
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </Col>
+      <Col md="3">
+        <div className={styles.footerSection}>
+          <div className={styles.footerIconText}>
+            <i className={`${styles.footerIcon} fa fa-phone`} />
+            <p className={styles.footerText}>
+              Helpline: +123456789
             </p>
-            <Link to="/register" className={styles.footerButton}>
-              Join now!
-            </Link>
           </div>
-        </Col>
-        <Col md="6">
-          <div className={styles.footerSection}>
-            <h5 className={styles.footerSectionTitle}>Follow us</h5>
-            <ul className={`list-inline ${styles.socialIcons}`}>
-              <li className={styles.socialIcon}>
-                <a href="https://www.instagram.com">
-                  <i className="fa fa-instagram"></i>
-                </a>
-              </li>
-              <li className={styles.socialIcon}>
-                <a href="https://www.facebook.com">
-                  <i className="fa fa-facebook"></i>
-                </a>
-              </li>
-              <li className={styles.socialIcon}>
-                <a href="https://twitter.com/">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </Col>
-      </Row>
+          <p className={styles.footerText}>
+            Monday - Friday from 8:00 am - 4:00 pm
+          </p>
+        </div>
+      </Col>
+    </Row>
 
-      <Row>
-        <Col md="12" className={styles.footerText}>
-          <small className={styles.footerCopyright}>
-            &copy; 2023 camON Store. All rights reserved. |{' '}
-            <Link to="/rules">Terms and Conditions</Link> |{' '}
-            <Link to="/privacy-policy">Privacy Policy</Link> |{' '}
-            <Link to="/returns">Returns</Link> |{' '}
-            <Link to="/contact">Contact</Link>
-          </small>
-        </Col>
-      </Row>
-    </Container>
+    <Row>
+      <Col md="12">
+        <ul className={styles.socialIcons}>
+          <li>
+            <a href="https://www.instagram.com">
+              <i className="fa fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com">
+              <i className="fa fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </li>
+        </ul>
+      </Col>
+    </Row>
+
+    <Row>
+      <Col md="12" className={styles.footerText}>
+        <small className={styles.footerCopyright}>
+          &copy; 2023 camON Store. All rights reserved.
+        </small>
+      </Col>
+    </Row>
+
   </footer>
 );
 
